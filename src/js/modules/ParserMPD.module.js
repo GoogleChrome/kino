1acd076f8e6c55854b16c55abb0981cbfcc49edd
Jaroslav Polakovič <jaroslav.polakovic@xwp.co>
2021-02-22 17:11:42 +0100

    Implement DASH adaptive streaming.

--- src/js/modules/ParserMPD.module.js
+++ src/js/modules/ParserMPD.module.js
@@ -1,5 +1,117 @@
 import iso8601TimeDurationToSeconds from './Duration.module';
 
+/**
+ * Replaces MPD variables in the chunk URL string with proper values.
+ *
+ * @param {string} chunkURL             URL of the chunk file with MPD variable intact.
+ * @param {object} representationObject Representation object.
+ * @param {number} chunkIndex           Index of the chunk to be returned.
+ *
+ * @returns {string} Interpolated chunk URL.
+ */
+const interpolateChunkURL = (chunkURL, representationObject, chunkIndex = 0) => {
+  chunkIndex = chunkIndex.toString();
+
+  const replaceTuples = [
+    [/\$RepresentationID\$/, representationObject.id],
+    [/\$Number%([0-9]+)d\$/, (match, repeats) => chunkIndex.padStart(parseInt(repeats, 10), 0)],
+  ];
+
+  replaceTuples.forEach(
+    ([from, to]) => {
+      chunkURL = chunkURL.replace(from, to);
+    },
+  );
+  return chunkURL;
+};
+
+/**
+ * Returns the segment filename based on the index requested.
+ *
+ * @param {object} representationObject Representation object.
+ * @param {number} index                Chunk index.
+ *
+ * @returns {string} Interpolated chunk filename.
+ */
+const getSegmentByIndex = (representationObject, index) => {
+  const segmentTemplate = representationObject.element.querySelector('SegmentTemplate');
+  const chunkURLTemplate = segmentTemplate.getAttribute('media');
+
+  return interpolateChunkURL(chunkURLTemplate, representationObject, index);
+};
+
+/**
+ * Returns the initial segment filename.
+ *
+ * @param {object} representationObject Representation object.
+ *
+ * @returns {string} Interpolated chunk filename.
+ */
+const getInitialSegment = (representationObject) => {
+  const segmentTemplate = representationObject.element.querySelector('SegmentTemplate');
+  const chunkURLTemplate = segmentTemplate.getAttribute('initialization');
+
+  return interpolateChunkURL(chunkURLTemplate, representationObject);
+};
+
+const getSegmentIndexByTime = (representationObject, time) => {
+  const segmentTemplate = representationObject.element.querySelector('SegmentTemplate');
+  const segmentTimeline = segmentTemplate.querySelector('SegmentTimeline');
+  const segmentElements = [...segmentTimeline.querySelectorAll('S')];
+  const timescale = parseInt(segmentTemplate.getAttribute('timescale'), 10);
+
+  if (!timescale) return null;
+
+  const requestedTimeInTimescale = time * timescale;
+
+  let currentStart = 0;
+  let currentEnd = 0;
+  let currentIndex = 1;
+  let returnIndex = null;
+
+  segmentElements.forEach(
+    (segmentEl) => {
+      const duration = Number(segmentEl.getAttribute('d'));
+      const repeats = Number(segmentEl.getAttribute('r') || '0') + 1;
+
+      currentStart = Number(segmentEl.getAttribute('t')) || currentStart;
+      currentEnd = currentStart + (duration * repeats);
+
+      if (requestedTimeInTimescale >= currentStart && requestedTimeInTimescale < currentEnd) {
+        const timeIntoSegment = requestedTimeInTimescale - currentStart;
+        returnIndex = currentIndex + Math.floor(timeIntoSegment / duration);
+      }
+
+      currentIndex += repeats;
+      currentStart = currentEnd;
+    },
+  );
+
+  return returnIndex;
+};
+
+const representationElementToObject = (representationElement) => {
+  const representation = [...representationElement.attributes].reduce(
+    (carry, attr) => {
+      carry[attr.name] = attr.value;
+      return carry;
+    },
+    {},
+  );
+
+  representation.element = representationElement;
+  representation.maxChunkIndex = [...representationElement.querySelectorAll('S')].reduce(
+    (chunkIndex, segmentEl) => chunkIndex + 1 + Number(segmentEl.getAttribute('r') || '0'),
+    0,
+  );
+
+  representation.getInitialSegment = getInitialSegment.bind(null, representation);
+  representation.getSegmentByIndex = getSegmentByIndex.bind(null, representation);
+  representation.getSegmentIndexByTime = getSegmentIndexByTime.bind(null, representation);
+
+  return representation;
+};
+
 export default class {
   constructor(manifest) {
     const domParser = new DOMParser();
@@ -18,6 +130,7 @@ export default class {
 
     // Public properties.
     this.minBufferTime = this.getMinBufferTime();
+
     this.duration = this.getDuration();
   }
 
@@ -43,16 +156,8 @@ export default class {
     if (!adaptationSet) return [];
 
     const representationElements = [...adaptationSet.querySelectorAll(`Representation${representationQuery}`)];
+    const representationObjects = representationElements.map(representationElementToObject);
 
-    return representationElements.map(
-      // For each <Representation> return an object mapping its attributes to values.
-      (representation) => [...representation.attributes].reduce(
-        (carry, attr) => {
-          carry[attr.name] = attr.value;
-          return carry;
-        },
-        {},
-      ),
-    );
+    return representationObjects;
   }
 }
--- src/js/modules/Streamer.module.js
+++ src/js/modules/Streamer.module.js
@@ -8,7 +8,7 @@ const DEFAULT_AUDIO_PRIORITIES = [
   '[mimeType="audio/mp4"]',
 ];
 
-const STREAM_TYPES = ['audio', 'video'];
+const ALL_STREAM_TYPES = ['audio', 'video'];
 
 /**
  * Streams raw media data to the <video> element.
@@ -21,7 +21,9 @@ export default class {
     this.stream = {
       media: {
         baseURL: '/',
+        streamTypes: [],
         representations: this.initRepresentations(),
+        lastRepresentationsIds: {},
         duration: this.parser.duration,
       },
       buffer: {
@@ -37,14 +39,15 @@ export default class {
       measuredDownlink: null,
     };
 
+    // Iterate which stream types we have separate representations for.
+    this.stream.media.streamTypes = Object.keys(this.stream.media.representations);
+
     if (this.opts.manifestSrc) {
       const manifestURL = new URL(this.opts.manifestSrc);
       this.stream.media.baseURL = `${manifestURL.origin}${manifestURL.pathname.replace(/[^/]+$/, '')}`;
     }
 
-    this.initializeBuffer().then(() => {
-      this.observeVideoUpdates();
-    });
+    this.initializeStream();
   }
 
   getDownlink() {
@@ -82,9 +85,13 @@ export default class {
         const previousBandwidth = parseInt(previous.bandwidth, 10);
 
         const currentIsHigherQuality = currentBandwidth > previousBandwidth;
-        const currentFitsDownlink = (currentBandwidth + usedBandwidth) < downlinkInBits;
+        const currentFitsDownlink = (currentBandwidth + usedBandwidth) <= downlinkInBits;
+        const previousDoesntFitDownlink = (previousBandwidth + usedBandwidth) > downlinkInBits;
+        const currentIsLessData = currentBandwidth < previousBandwidth;
 
-        return (currentIsHigherQuality && currentFitsDownlink) ? current : previous;
+        if (currentIsHigherQuality && currentFitsDownlink) return current;
+        if (previousDoesntFitDownlink && currentIsLessData) return current;
+        return previous;
       },
       this.stream.media.representations.video[0],
     );
@@ -107,7 +114,7 @@ export default class {
      * Select sets of video and audio representations that are playable
      * in the client with respect to indicated priorities.
      */
-    STREAM_TYPES.forEach(
+    ALL_STREAM_TYPES.forEach(
       (contentType) => {
         let query;
         do {
@@ -118,7 +125,6 @@ export default class {
            */
           representations[contentType] = this.parser.queryRepresentations(query, contentType, 'eng');
           representations[contentType] = representations[contentType].filter(canPlayFilter);
-          if (representations[contentType]) break;
         } while (query);
       },
     );
@@ -152,32 +158,191 @@ export default class {
     }
   }
 
-  initializeBuffer() {
-    return new Promise((resolve) => {
-      this.stream.buffer.mediaSource = new MediaSource();
-      this.videoEl.src = URL.createObjectURL(this.stream.buffer.mediaSource);
+  async initializeStream() {
+    await this.initializeMediaSource();
+    this.initializeBuffers();
+    this.observeVideoUpdates();
+  }
 
-      this.stream.buffer.mediaSource.addEventListener('sourceopen', (e) => {
-        const mediaSource = e.target;
-        STREAM_TYPES.forEach((type) => {
-          if (this.stream.media.representations[type].length) {
-            const mimeString = this.getRepresentationMimeString(
-              this.stream.media.representations[type][0],
-            );
-            this.stream.buffer.sourceBuffers[type] = mediaSource.addSourceBuffer(mimeString);
-            /**
-             * const representations = this.getRepresentationsByBandwidth();
-             * console.log(representations);
-             */
-          }
-        });
+  /**
+   * Initializes the Media Source and appends it to a video element.
+   *
+   * @returns {Promise} Promise that resolves with opened MediaSource instance.
+   */
+  initializeMediaSource() {
+    const mediaSource = new MediaSource();
+    this.videoEl.src = URL.createObjectURL(mediaSource);
+
+    return new Promise((resolve) => {
+      mediaSource.addEventListener('sourceopen', (e) => {
+        this.stream.buffer.mediaSource = e.target;
+        this.stream.buffer.mediaSource.duration = this.stream.media.duration;
         resolve();
       });
     });
   }
 
+  initializeSourceBuffer(mimeString) {
+    const sourceBuffer = this.stream.buffer.mediaSource.addSourceBuffer(mimeString);
+    sourceBuffer.dataQueue = {
+      internal: {
+        entries: [],
+        updating: false,
+      },
+      append(data) {
+        this.internal.entries.push(data);
+        if (!this.internal.updating) {
+          this.step();
+        }
+      },
+      step() {
+        const data = this.internal.entries.shift();
+        if (data) {
+          this.internal.updating = true;
+          sourceBuffer.appendBuffer(data);
+        }
+      },
+    };
+
+    sourceBuffer.fileQueue = {
+      internal: {
+        files: [],
+        bufferedFiles: new Set(),
+        reading: false,
+      },
+      add(fileObj) {
+        if (!fileObj.force && this.internal.bufferedFiles.has(fileObj.id)) return;
+
+        this.internal.files.push(fileObj.url);
+        this.internal.bufferedFiles.add(fileObj.id);
+
+        if (!this.internal.reading) this.step();
+      },
+      step() {
+        const filename = this.internal.files.shift();
+        if (!filename) return;
+
+        this.internal.reading = true;
+        fetch(filename).then((response) => response.body).then(
+          async (body) => {
+            const reader = body.getReader();
+            const readChunk = async () => {
+              const { value, done } = await reader.read();
+              return [value, done];
+            };
+
+            let data;
+            let done;
+
+            /* eslint-disable no-await-in-loop */
+            do {
+              [data, done] = await readChunk(); // await here is fine, the process is sequential.
+              if (!done) sourceBuffer.enqueueAppendBuffer(data);
+            } while (!done);
+            /* eslint-enable no-await-in-loop */
+
+            this.internal.reading = false;
+            this.step();
+          },
+        );
+      },
+    };
+
+    sourceBuffer.addEventListener('updateend', (e) => {
+      e.target.dataQueue.internal.updating = false;
+      e.target.dataQueue.step();
+    });
+    sourceBuffer.enqueueAppendBuffer = sourceBuffer.dataQueue.append.bind(sourceBuffer.dataQueue);
+    sourceBuffer.enqueueFile = sourceBuffer.fileQueue.add.bind(sourceBuffer.fileQueue);
+
+    return sourceBuffer;
+  }
+
+  initializeBuffers() {
+    const { streamTypes, representations } = this.stream.media;
+
+    streamTypes.forEach((streamType) => {
+      const mimeString = this.getRepresentationMimeString(representations[streamType][0]);
+      this.stream.buffer.sourceBuffers[streamType] = this.initializeSourceBuffer(mimeString);
+    });
+
+    this.bufferAhead();
+  }
+
+  getBufferHealth(buffer) {
+    const currentPlaybackTime = this.videoEl.currentTime;
+    const { healthyDuration } = this.stream.buffer;
+    const bufferedTimeRanges = buffer.buffered;
+
+    let bufferLeft = 0;
+    let bufferEndTime = currentPlaybackTime;
+
+    for (let i = 0; i < bufferedTimeRanges.length; i += 1) {
+      const start = bufferedTimeRanges.start(i);
+      const end = bufferedTimeRanges.end(i);
+
+      if (currentPlaybackTime >= start && currentPlaybackTime <= end) {
+        bufferLeft = end - currentPlaybackTime;
+        bufferEndTime = end;
+      }
+    }
+
+    return {
+      isHealthy: bufferLeft >= healthyDuration,
+      bufferEndTime,
+    };
+  }
+
   bufferAhead() {
-    // const currentPlaybackTime = this.videoEl.currentTime;
     this.throttleBuffer();
+
+    const currentRepresentations = this.getRepresentationsByBandwidth();
+    const { baseURL, streamTypes, lastRepresentationsIds } = this.stream.media;
+
+    streamTypes.forEach((streamType) => {
+      const buffer = this.stream.buffer.sourceBuffers[streamType];
+      const bufferHealth = this.getBufferHealth(buffer);
+
+      if (bufferHealth.isHealthy) {
+        return;
+      }
+
+      const representation = currentRepresentations[streamType];
+      const chunkIndex = representation.getSegmentIndexByTime(bufferHealth.bufferEndTime);
+
+      if (!chunkIndex) {
+        return;
+      }
+
+      const lastRepresentationId = lastRepresentationsIds[streamType];
+      const filesToBuffer = [];
+
+      if (representation.id !== lastRepresentationId) {
+        filesToBuffer.push({
+          id: `initial-${representation.id}`,
+          url: `${baseURL}${representation.getInitialSegment()}`,
+          force: true,
+        });
+      }
+      this.stream.media.lastRepresentationsIds[streamType] = representation.id;
+
+      const chunkIndicesToLoad = [chunkIndex];
+      if (chunkIndex < representation.maxChunkIndex) {
+        chunkIndicesToLoad.push(chunkIndex + 1);
+      }
+
+      chunkIndicesToLoad.forEach(
+        (index) => {
+          filesToBuffer.push({
+            id: index,
+            url: `${baseURL}${representation.getSegmentByIndex(index)}`,
+          });
+        },
+      );
+
+      filesToBuffer.map(
+        (fileObj) => buffer.enqueueFile(fileObj),
+      );
+    });
   }
 }
