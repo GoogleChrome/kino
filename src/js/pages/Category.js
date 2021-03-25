e1fa8520d4396f8d78e5dfa8cf8bd0f4a5443f2f
Derek Herman <derek.herman@xwp.co>
2021-03-24 18:32:09 -0700

    Move function out of the component

--- src/js/pages/Category.js
+++ src/js/pages/Category.js
@@ -1,27 +1,5 @@
-import slugify from '../utils/slugify';
+import findCategoryNameBySlug from '../utils/findCategoryNameBySlug';
 import appendVideoToGallery from '../utils/appendVideoToGallery';
-
-/**
- * Finds category name by slug (reverse lookup).
- *
- * @todo Later on if we introduce Categories endpoint that will be changed.
- *
- * @param {string} slug Slug.
- * @param {object[]} apiData Array of video metadata objects.
- *
- * @returns {string} Category name or empty string.
- */
-function findCategoryNameBySlug(slug, apiData) {
-  for (let i = 0; i < apiData.length; i += 1) {
-    for (let j = 0; j < apiData[i].categories.length; j += 1) {
-      const cat = apiData[i].categories[j];
-      if (slugify(cat) === slug) {
-        return cat;
-      }
-    }
-  }
-  return '';
-}
 
 /**
  * @param {RouterContext} routerContext Context object passed by the Router.
