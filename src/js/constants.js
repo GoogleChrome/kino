1be2247258db539824f8da1554410532e49eb02a
Derek Herman <derek.herman@xwp.co>
2021-05-29 23:30:28 -0700

    App manifest, plus icons

--- src/js/constants.js
+++ src/js/constants.js
@@ -62,12 +62,12 @@ export const IDB_CHUNK_INDEX = 'video-chunk';
  * Default artwork metadata used by Media Session API.
  */
 export const MEDIA_SESSION_DEFAULT_ARTWORK = [
-  { src: '/images/media-session/artwork-96x96.jpg', sizes: '96x96', type: 'image/jpeg' },
-  { src: '/images/media-session/artwork-128x128.jpg', sizes: '128x128', type: 'image/jpeg' },
-  { src: '/images/media-session/artwork-192x192.jpg', sizes: '192x192', type: 'image/jpeg' },
-  { src: '/images/media-session/artwork-256x256.jpg', sizes: '256x256', type: 'image/jpeg' },
-  { src: '/images/media-session/artwork-384x384.jpg', sizes: '384x384', type: 'image/jpeg' },
-  { src: '/images/media-session/artwork-512x512.jpg', sizes: '512x512', type: 'image/jpeg' },
+  { src: '/images/icon-96x96.png', sizes: '96x96', type: 'image/png' },
+  { src: '/images/icon-128x128.png', sizes: '128x128', type: 'image/png' },
+  { src: '/images/icon-192x192.png', sizes: '192x192', type: 'image/png' },
+  { src: '/images/icon-256x256.png', sizes: '256x256', type: 'image/png' },
+  { src: '/images/icon-384x384.png', sizes: '384x384', type: 'image/png' },
+  { src: '/images/icon-512x512.png', sizes: '512x512', type: 'image/png' },
 ];
 
 /**
