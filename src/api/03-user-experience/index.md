7aead9ac3b515b68f86ba268f4de86e4bd843733
Jaroslav Polakovič <jaroslav.polakovic@xwp.co>
2021-12-09 13:48:04 +0100

    Remove the autoplay post.

--- src/api/03-user-experience/index.md
+++
@@ -1,6 +0,0 @@
----
-name: User Experience
-slug: user-experience
----
-
-Unleash your videos and learn how autoplay, picture-in-picture and casting can be implemented in ways that improve user experience.
--- src/js/sw/cache.js
+++ src/js/sw/cache.js
@@ -43,5 +43,4 @@ export default [
   'https://storage.googleapis.com/kino-assets/streaming-basics/thumbnail.png',
   'https://storage.googleapis.com/kino-assets/efficient-formats/thumbnail.png',
   'https://storage.googleapis.com/kino-assets/adaptive-streaming/thumbnail.png',
-  'https://storage.googleapis.com/kino-assets/autoplay/thumbnail.png',
 ];
